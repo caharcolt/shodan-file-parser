05a403c760f768ab45dc6b57ddd2176aded7dd47
caharcolt <43115602+caharcolt@users.noreply.github.com>
2022-06-09 20:16:49 +0300

    Update parser.py
    add amount of strings

--- parse/parser.py
+++ parse/parser.py
@@ -3,16 +3,20 @@ import sys
 
 #Get ip only
 def get_ip():
+    amount=0
     filename = sys.argv[2]
     input_file = open (filename, "r")
     output_file = open("output.txt", "w")
     for line in input_file:
+        amount = amount+1
         jsonObj = json.loads(line)
         ip = jsonObj.get ('ip_str')
         output_file.write(f"{ip}\n")
+    print (f"\nDone! \nSorted {amount} lines")
 
 #Get ports only
 def get_port():
+    amount=0
     filename = sys.argv[2]
     input_file = open (filename, "r")
     output_file = open("output.txt", "w")
@@ -20,16 +24,20 @@ def get_port():
         jsonObj = json.loads(line)
         port = jsonObj.get ('port')
         output_file.write(f"{port}\n")
+    print (f"\nDone! \nSorted {amount} lines")
 
 #Get ip:port
 def get_ip_port():
+    amount=0
     filename = sys.argv[2]
     input_file = open (filename, "r")
     output_file = open("output.txt", "w")
     for line in input_file:
+        amount = amount+1
         jsonObj = json.loads(line)
         ip = jsonObj.get ('ip_str')
         port = jsonObj.get('port')
         output_file.write(f"{ip}:{port}\n")
+    print (f"\nDone! \nSorted {amount} lines")
 
 
